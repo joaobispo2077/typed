6f6ffec56401010f77f2635329f1dbe9629f106a
João Bispo <vitor.vi10@hotmail.com>
2021-04-11 17:32:31 -0300

    review this difference in arrow function and classic function, review defaul params in functions

--- ecmascript/index.js
+++ ecmascript/index.js
@@ -14,4 +14,41 @@ function revel() {
 }
 revel(); // shadowing
 console.log(secret);
+// Arrow function
+const sumNum = function (a, b) {
+    return a + b;
+};
+const subtract = (a, b) => a - b;
+console.log(subtract(10, 2));
+const sayHello = () => console.log('Hello!');
+sayHello();
+const talkWithPeople = (people) => console.log(`Hi ${people}`);
+talkWithPeople('Sofia');
+// this with arrow  fn and normal fn
+//  function normalThis() {
+//    console.log(this);
+//  }
+// normalThis();
+// const normalWithSpecialThis = normalThis.bind(normalThis);
+// normalWithSpecialThis();
+// const arrowWithThis = (): void => console.log(this);
+// arrowWithThis();
+// const arrowWithSpecialThis = arrowWithThis.bind(arrowWithThis);
+// arrowWithSpecialThis();
+/*
+Em funções tradicionais o this pode variar de acordo com a chamada da função
+mas já em arrow function o this sempre aponta para o contexto em que ela foi definida
+*/
+// Default Params
+const regressiveClock = (initialTime = 3, finalTime = initialTime - 5) => {
+    console.log(initialTime);
+    while (initialTime > finalTime) {
+        --initialTime;
+        console.log(initialTime);
+    }
+    console.log(`Fim da contagem regressiva com um tempo inicial de ${initialTime} e tempo final de ${finalTime}`);
+};
+regressiveClock();
+regressiveClock(3);
+regressiveClock(5, 0);
 //# sourceMappingURL=index.js.map
--- ecmascript/index.ts
+++ ecmascript/index.ts
@@ -19,3 +19,60 @@ console.log(isPossible);
 
  revel(); // shadowing
  console.log(secret);
+
+ // Arrow function
+
+ const sumNum = function(a: number, b: number): number {
+   return a + b;
+ }
+
+ const subtract = (a: number, b: number): number => a - b;
+
+ console.log(subtract(10,2));
+
+ const sayHello = (): void => console.log('Hello!');
+
+ sayHello();
+
+ const talkWithPeople = (people: string): void => console.log(`Hi ${people}`);
+
+ talkWithPeople('Sofia');
+// this with arrow  fn and normal fn
+//  function normalThis() {
+//    console.log(this);
+//  }
+
+// normalThis();
+
+// const normalWithSpecialThis = normalThis.bind(normalThis);
+
+// normalWithSpecialThis();
+
+// const arrowWithThis = (): void => console.log(this);
+
+// arrowWithThis();
+
+// const arrowWithSpecialThis = arrowWithThis.bind(arrowWithThis);
+
+// arrowWithSpecialThis();
+/*
+Em funções tradicionais o this pode variar de acordo com a chamada da função
+mas já em arrow function o this sempre aponta para o contexto em que ela foi definida
+*/
+
+// Default Params
+
+const regressiveClock = (initialTime: number = 3, finalTime: number = initialTime - 5): void => {
+  console.log(initialTime);
+
+  while(initialTime > finalTime){  
+    --initialTime;
+    console.log(initialTime);
+  }
+
+  console.log(`Fim da contagem regressiva com um tempo inicial de ${initialTime} e tempo final de ${finalTime}`);
+}
+
+regressiveClock();
+regressiveClock(3);
+regressiveClock(5, 0);
