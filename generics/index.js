8b8834ecf4520227d63c1bda57b00a08ad8a5931
João Bispo <joaobispo2077@gmail.com>
2021-04-25 12:22:49 -0300

    learn: generics with array api

--- generics/index.js
+++ generics/index.js
@@ -18,4 +18,29 @@ console.log(echoG('Teste').length);
 console.log(echoG({ name: 'John', age: 21 }).name);
 console.log(echoG({ catname: 'Mia', age: 7 }));
 console.log(echoG([{ catname: 'Mia', age: 7 }, { catname: 'Doug', age: 4 }]).length);
+// Generics Disponíveis na API
+// const assessments: number[] = [2, 10, 3.9, 7.7];
+//-- assign type in a generic constructor as class array
+const assessments = [2, 10, 3.9, 7.7];
+assessments.push(8.2);
+assessments.push('5.9');
+console.log(assessments);
+// Array with generics
+function logger(args) {
+    args.forEach(element => console.log(element));
+    return args;
+}
+logger([1, 2, 3]); // Array de números Genérico por inferência
+logger([2, 1]); // Array de números Genérico por referência
+logger(['Joao', 'Sofia', 'Vivi']);
+logger(['Joao', 2, 'Vivi']); // Array de Union Type (string and number)
+logger([{ name: 'John', age: 21 },
+    { name: 'John', age: 21 },
+    { name: 'John', age: 21 }]);
+logger([{ name: 'John', age: 21 },
+    { name: 'John', age: 21 },
+    { name: 'John', age: 21 }]);
+logger([{ name: 'John', age: 21 },
+    { name: 'John', age: 21 },
+    { name: 'John', age: 21 }]);
 //# sourceMappingURL=index.js.map
--- generics/index.ts
+++ generics/index.ts
@@ -21,3 +21,53 @@ console.log(echoG('Teste').length);
 console.log(echoG({ name: 'John',  age: 21}).name);
 console.log(echoG({ catname: 'Mia',  age: 7}));
 console.log(echoG([{ catname: 'Mia',  age: 7}, { catname: 'Doug',  age: 4}]).length);
+
+// Generics Disponíveis na API
+
+// const assessments: number[] = [2, 10, 3.9, 7.7];
+//-- assign type in a generic constructor as class array
+const assessments: Array<number> = [2, 10, 3.9, 7.7];
+assessments.push(8.2);
+assessments.push('5.9');
+console.log(assessments);
+
+
+// Array with generics
+function logger<T>(args: T[]): T[] {
+    args.forEach(element => console.log(element));
+    return args;
+}
+
+logger([1,2,3]); // Array de números Genérico por inferência
+logger<number>([2, 1]); // Array de números Genérico por referência
+logger<string>(['Joao', 'Sofia', 'Vivi']);
+logger<string | number>(['Joao', 2, 'Vivi']); // Array de Union Type (string and number)
+
+logger<{name: string, age: number}>(
+  [{ name: 'John', age: 21},
+  { name: 'John', age: 21},
+  { name: 'John', age: 21}]
+);
+
+type Student = {
+  name: string;
+  age: number
+}
+
+logger<Student>(
+  [{ name: 'John', age: 21},
+  { name: 'John', age: 21},
+  { name: 'John', age: 21}]
+);
+
+interface IStudent {
+  name: string;
+  age: number;
+}
+
+logger<IStudent>(
+  [{ name: 'John', age: 21},
+  { name: 'John', age: 21},
+  { name: 'John', age: 21}]
+);
+
