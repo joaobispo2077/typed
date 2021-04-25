c0d080a23d50291e5d420c68e303d53411520390
João Bispo <joaobispo2077@gmail.com>
2021-04-25 11:57:33 -0300

    learn: implement generic syntax into a function

---
+++ generics/index.ts
@@ -0,0 +1,23 @@
+
+// trying be generic without Generics
+function rawEcho(obj: any): any {
+  return obj;
+}
+
+console.log(rawEcho(2).length); // sem problema na compilação do TS, mas gera undefined em tempo de execução
+console.log(rawEcho('Teste').length);
+console.log(rawEcho({ name: 'John',  age: 21}));
+console.log(rawEcho({ catname: 'Mia',  age: 7}));
+console.log(rawEcho([{ catname: 'Mia',  age: 7}, { catname: 'Doug',  age: 4}]));
+
+// trying be generic with Generics
+function echoG<T>(obj: T): T {
+  return obj;
+}
+
+console.log(echoG(2).length); // erro na compilação do TS
+console.log(echoG<number>(2)); // forçar que o Generic assuma um Tipo
+console.log(echoG('Teste').length);
+console.log(echoG({ name: 'John',  age: 21}).name);
+console.log(echoG({ catname: 'Mia',  age: 7}));
+console.log(echoG([{ catname: 'Mia',  age: 7}, { catname: 'Doug',  age: 4}]).length);
