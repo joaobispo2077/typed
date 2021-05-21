f75312bd612730ad16c6db34fd8e129578b09bd9
João Bispo <joaobispo2077@gmail.com>
2021-05-21 11:22:35 -0300

    feat: added namespaces

---
+++ src/namespaces/A0058-namespaces/index.ts
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-namespace*/
+namespace Space {
+  export const name = 'Joao';
+
+  export class Person {
+    constructor(public name: string) {}
+  }
+
+  const person = new Person(name);
+  console.log(person);
+
+  export namespace OtherSpace {
+    export const lastname = 'Bispo';
+  }
+}
+
+const person2 = new Space.Person('Batatinha');
+console.log(person2);
+console.log(Space.OtherSpace.lastname);
+
+export default 1;
